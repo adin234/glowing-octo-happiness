53dae57857fa471093a249d08f377d6ae11f25c6
airondumael <airon.dumael@gmail.com>
2014-10-01 21:40:08 +0800

    x

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
-var server 				= 'http://54.255.176.250/';
-var origin				= 'http://beta.gamers.tm/';
-var attachments_server 	= 'http://community.gamers.tm/zh/';
-var community			= 'http://community.gamers.tm/zh/';
+var server              = 'http://54.255.176.250/';
+var origin              = 'http://beta.gamers.tm/';
+var attachments_server  = 'http://community.gamers.tm/zh/';
+var community           = 'http://community.gamers.tm/zh/';
 
 var utilLoader = {
     show: function(loader) {
