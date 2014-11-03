cc88865a1d610f565fe21ebb73ded8fd530c540d
airondumael <airon.dumael@gmail.com>
2014-11-04 05:37:31 +0800

    x

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
-var server              = 'http://54.255.176.250/';
+var server              = 'http://localhost:3000/';
 var origin              = 'http://localhost:8000/';
 var attachments_server  = 'http://community.gamers.tm/zh/';
 var community           = 'http://community.gamers.tm/zh/';
