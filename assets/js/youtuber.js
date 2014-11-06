5a15a5bcf718b53e4d611ebdaf53b77c081456e1
airondumael <airon.dumael@gmail.com>
2014-11-07 02:08:27 +0800

    x

--- assets/js/youtuber.js
+++ assets/js/youtuber.js
@@ -52,9 +52,12 @@ $.get(server+'streamers?user='+page_data.user.user_id, function(result) {
 });
 
 $.get(server+'streamers/youtube?user='+page_data.user.user_id, function(result) {
-	if(!liveStreamLink) {
-		// liveStreamLink = '/gamer_stream/'+page_data.user.user_id+'/'+'YT'+result.streamers[0].youtube.id;
-		// $('.live-button').attr('href', liveStreamLink).show();
+	if(result.streamers.length) {
+		liveStreamLink = '/gamer_stream/?user='+page_data.user.user_id+'/'+'YT'+result.streamers[0].youtube.id;
+	}
+
+	if(liveStreamLink) {
+		$('.live-button').attr('href', liveStreamLink).show();
 	}
 });
 
