b5134f356c0ad8f126b2f75c9272a86e6010c46d
airondumael <airon.dumael@gmail.com>
2014-11-17 22:29:14 +0800

    yt streamers

--- assets/js/youtuber.js
+++ assets/js/youtuber.js
@@ -60,7 +60,7 @@ $.get(server+'streamers?user='+page_data.user.user_id, function(result) {
 
 $.get(server+'streamers/youtube?user='+page_data.user.user_id, function(result) {
 	if(result.streamers.length) {
-		liveStreamLink = '/gamer_stream/?user='+page_data.user.user_id+'/'+'YT'+result.streamers[0].youtube.id;
+		liveStreamLink = '/gamer_stream/?user='+page_data.user.user_id+'/#!/'+'YT'+result.streamers[0].youtube.id;
 	}
 
 	if(liveStreamLink) {
