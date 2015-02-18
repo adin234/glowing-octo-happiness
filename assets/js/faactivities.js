bccb64ffb2affbdc4cc0def935f93141ddeba60e
nesse <jdeburce@gmail.com>
2015-02-18 14:39:49 +0800

    Freedom Schedules

--- assets/js/faactivities.js
+++ assets/js/faactivities.js
@@ -28,6 +28,7 @@ console.log(data);
 				'end_date' : $('#event_end_date').val(),
 				'start_time' : $('#event_start_time').val(),
 				'end_time' : $('#event_end_time').val(),
+				'e_description' : $('#event_desc').val()
 			}
 
 	}).success(function (data){
@@ -45,40 +46,39 @@ console.log(data);
 function get_events() {
 
 var eventsHtml = []; 
-var title, startDate, endDate, startTime, endTime; 
+var title, startDate, endDate, startTime, endTime, eDesc; 
 
-	$.ajax({
-			dataType: 'json',
-			url: server+'freedom_events',
-			type: 'get',
-			data: {
+					$.ajax({
+							dataType: 'json',
+							url: server+'freedom_events',
+							type: 'get',
+							data: {
 
-				'event_title': $('#event_name').val(),
-				'start_date' : $('#event_start_date').val(),
-				'end_date' : $('#event_end_date').val(),
-				'start_time' : $('#event_start_time').val(),
-				'end_time' : $('#event_end_time').val(),
-			}
+								'event_title': $('#event_name').val(),
+								'start_date' : $('#event_start_date').val(),
+								'end_date' : $('#event_end_date').val(),
+								'start_time' : $('#event_start_time').val(),
+								'end_time' : $('#event_end_time').val(),
+								'e_description' : $('#event_desc').val()
+							}
 
-	}).success(function (data){
+					}).success(function (data){
 
-			
-		data.forEach(function(item){
+							
+						data.forEach(function(item){
+							
+							
 
-			
-			console.log(item.event_title);
-			console.log(item.start_date);
-			console.log(item.end_date);
-			console.log(item.start_time);
-			console.log(item.end_time);
-			
-			 title = item.event_title;
-			 startDate = item.start_date;
-			 endDate = item.end_date;
-			 startTime = item.start_time;
-			 endTime = item.end_time;
 
-		});	
+							 title = item.event_title;
+							 startDate = item.start_date;
+							 endDate = item.end_date;
+							 startTime = item.start_time;
+							 endTime = item.end_time;
+							 eDesc = item.e_description; 
+						});
+
+				
 
 
 
@@ -87,7 +87,8 @@ var title, startDate, endDate, startTime, endTime;
 				'<div id="startEventDate">' + startDate + '</div>' + 
 				'<div id="endEventDate">' + endDate + '</div>' + 
 				'<div id="startEventTime">' + startTime + '</div>' + 
-				'<div id="endEventTime">' + endTime + '</div>' 
+				'<div id="endEventTime">' + endTime + '</div>' +
+				'<div id="e-desc">' + eDesc + '</div>' 
 				);
 
 
@@ -150,3 +151,69 @@ function search_events() {
 
 	});
 } 
+
+
+
+function get_event_status(){
+
+	//get event status 
+
+	//flag - ended - 0, ongoing - 1
+}
+
+
+function get_schedule(){
+
+var startDate, endDate, startTime, endTime, eventTitle, eventStatus;
+
+	
+		$.ajax({
+							dataType: 'json',
+							url: server+'freedom_events',
+							type: 'get',
+							data: {
+
+								'event_title': $('#event_name').val(),
+								'start_date' : $('#event_start_date').val(),
+								'end_date' : $('#event_end_date').val(),
+								'start_time' : $('#event_start_time').val(),
+								'end_time' : $('#event_end_time').val(),
+								'e_description' : $('#event_desc').val()
+							}
+
+					}).success(function (data){
+
+								data.forEach(function (item){
+
+											 console.log(item.event_title);
+											 console.log(item.start_date);
+											 console.log(item.end_date);
+											 console.log(item.start_time);
+											 console.log(item.end_time);
+
+											 eventTitle = item.event_title; 
+											 startDate = item.start_date;
+											 endDate = item.end_date;
+											 startTime = item.start_time; 
+											 endTime = item.end_time; 
+
+
+								}); 
+
+								$('#all_schedule').html(
+										'<div class="activity">' + 
+										'<div id="startEventDate">' + startDate + '</div>' + '-' + '<div id="endEventDate">' + endDate + '</div>' + 
+										'<div id="startEventTime">' + startTime + '</div>' + '-' + '<div id="endEventTime">' + endTime + '</div>' +
+										'<div id="eventHeader">' + eventTitle + '</div>' + '<div id="e_status"></div>' +
+										'</div>'
+								);
+
+
+					}).fail(function(data){
+							console.log('There were no data fetched');
+					});
+
+
+
+			
+}
