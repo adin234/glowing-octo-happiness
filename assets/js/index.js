8816b16ef86115c745eeb676adc4e4df17617320
Dennis Lorenzana <delorenzana@hotmail.com>
2015-02-19 16:30:02 +0800

    homepage news and shows playlists

--- assets/js/index.js
+++ assets/js/index.js
@@ -491,79 +491,84 @@ var update_index = function(index_data) {
 
 var news_shows_playlists = function() {
   
-//    var html = [];
-//    var blocks = '';
-//    var max_items = 3;
-//    var visible_news_playlists = (typeof index_data.visible_news_playlists != 'undefined') ? index_data.visible_news_playlists.split(',') : [];
-//    var visible_shows_playlists = (typeof index_data.visible_shows_playlists != 'undefined') ? index_data.visible_shows_playlists.split(',') : [];
-//        
-//    index_data.news_playlists.forEach(function(playlist, index){
-//       if(visible_news_playlists.indexOf(playlist.id) == -1){
-//            return;
-//       }
-//       blocks = '<div id="tab-news-playlist-'+index+'"><ul class="list clearFix">';
-//       blocks += '</ul></div>';
-//       $('#news_shows_playlists_block').append(blocks);
-//       
-//       $.ajax({
-//            url: server+'news',
-//            dataType: 'json',
-//            async: false,
-//            data: { playlist: playlist.id },
-//            success: function(result) {
-//                result.items.forEach(function(item, i){
-//                    if(item.status.privacyStatus === 'public'){
-//                        if(i > max_items) return;
-//                        if(typeof item.snippet.thumbnails !== 'undefined'){
-//                            item.thumb = item.snippet.thumbnails.medium.url;
-//                        }else{
-//                            item.thumb = null;
-//                        }
-//                        item.title = item.snippet.title;
-//                        item.link = '/news/#!/video/'+item.snippet.resourceId.videoId;
-//                        $('#tab-news-playlist-'+index+' ul').append(template($('#newsShowsTpl').html(), item));
-//                    }
-//                });
-//            }
-//       });
-//       
-//       html.push('<li><h2><a href="#tab-news-playlist-'+index+'">'+playlist.snippet.title+'</a></h2></li>');
-//    });
-//    
-//    index_data.shows_playlists.forEach(function(playlist, index){
-//       if(visible_shows_playlists.indexOf(playlist.id) == -1){
-//            return;
-//       }
-//       blocks = '<div id="tab-shows-playlist-'+index+'"><ul class="list clearFix">';
-//       blocks += '</ul></div>';
-//       $('#news_shows_playlists_block').append(blocks);
-//       
-//       $.ajax({
-//            url: server+'shows',
-//            dataType: 'json',
-//            async: false,
-//            data: { playlist: playlist.id },
-//            success: function(result) {
-//                result.items.forEach(function(item, i){
-//                    if(item.status.privacyStatus === 'public'){
-//                        if(i > max_items) return;
-//                        if(typeof item.snippet.thumbnails !== 'undefined'){
-//                            item.thumb = item.snippet.thumbnails.medium.url;
-//                        }else{
-//                            item.thumb = null;
-//                        }
-//                        item.title = item.snippet.title;
-//                        item.link = '/shows/#!/video/'+item.snippet.resourceId.videoId;
-//                        $('#tab-shows-playlist-'+index+' ul').append(template($('#newsShowsTpl').html(), item));
-//                    }
-//                });
-//            }
-//       });
-//       
-//       html.push('<li><h2><a href="#tab-shows-playlist-'+index+'">'+playlist.snippet.title+'</a></h2></li>');
-//    });
-//
-//    $('#news_shows_playlists').html(html.join(''));
+    var html = [];
+    var blocks = '';
+    var max_items = 3;
+    var ctr = 1;
+    var visible_news_playlists = (typeof index_data.visible_news_playlists != 'undefined') ? index_data.visible_news_playlists.split(',') : [];
+    var visible_shows_playlists = (typeof index_data.visible_shows_playlists != 'undefined') ? index_data.visible_shows_playlists.split(',') : [];
+        
+    index_data.news_playlists.forEach(function(playlist, index){
+       if(visible_news_playlists.indexOf(playlist.id) == -1){
+            return;
+       }
+       blocks = '<div id="tab-news-playlist-'+index+'"><ul class="list clearFix">';
+       blocks += '</ul></div>';
+       $('#news_shows_playlists_block').append(blocks);
+       
+       $.ajax({
+            url: server+'news',
+            dataType: 'json',
+            async: true,
+            data: { playlist: playlist.id },
+            success: function(result) {
+                ctr = 1;
+                result.items.forEach(function(item, i){
+                    if(item.status.privacyStatus === 'public'){
+                        if(ctr > max_items) return;
+                        if(typeof item.snippet.thumbnails !== 'undefined'){
+                            item.thumb = item.snippet.thumbnails.medium.url;
+                        }else{
+                            item.thumb = null;
+                        }
+                        item.title = item.snippet.title;
+                        item.link = '/news/#!/video/'+item.snippet.resourceId.videoId;
+                        $('#tab-news-playlist-'+index+' ul').append(template($('#newsShowsTpl').html(), item));
+                        ctr++;
+                    }
+                });
+            }
+       });
+       
+       html.push('<li><h2><a href="#tab-news-playlist-'+index+'">'+playlist.snippet.title+'</a></h2></li>');
+    });
+    
+    index_data.shows_playlists.forEach(function(playlist, index){
+       if(visible_shows_playlists.indexOf(playlist.id) == -1){
+            return;
+       }
+       blocks = '<div id="tab-shows-playlist-'+index+'"><ul class="list clearFix">';
+       blocks += '</ul></div>';
+       $('#news_shows_playlists_block').append(blocks);
+       
+       $.ajax({
+            url: server+'shows',
+            dataType: 'json',
+            async: true,
+            data: { playlist: playlist.id },
+            success: function(result) {
+                ctr = 1;
+                result.items.forEach(function(item, i){
+                    if(item.status.privacyStatus === 'public'){
+                        if(ctr > max_items) return;
+                        if(typeof item.snippet.thumbnails !== 'undefined'){
+                            item.thumb = item.snippet.thumbnails.medium.url;
+                        }else{
+                            item.thumb = null;
+                        }
+                        item.title = item.snippet.title;
+                        item.link = '/shows/#!/video/'+item.snippet.resourceId.videoId;
+                        $('#tab-shows-playlist-'+index+' ul').append(template($('#newsShowsTpl').html(), item));
+                        ctr++;
+                    }
+                });
+            }
+       });
+       
+       html.push('<li><h2><a href="#tab-shows-playlist-'+index+'">'+playlist.snippet.title+'</a></h2></li>');
+    });
+
+    $('#news_shows_playlists').html(html.join(''));
     //$('#news_shows_playlists_block').append(blocks);
     $(".sf-menu").superfish();
     $(".tabs").tabslet({
