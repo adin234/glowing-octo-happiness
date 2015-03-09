b4e948032b64868eaab436d1ab94d3b9316fda8a
wilbertverayin <wilbertverayin@gmail.com>
2015-03-09 15:03:18 +0800

    201503060004: YouTube stream in Multiview still get random link.

--- assets/js/gamer_stream_multi.js
+++ assets/js/gamer_stream_multi.js
@@ -195,6 +195,8 @@ $(function () {
         $('.chat-' + id.substr(2)).remove();
         utilHash.removeHash(id);
         $(this).parent().parent().remove();
+        console.log("getting information...");
+        window.history.pushState('', '', '/gamer_stream_multi');
     });
 
     $('#streamContainer').on('click', 'li a:not(.current)', function (e) {
@@ -202,6 +204,8 @@ $(function () {
         var id = $(this).attr('data-id');
         utilHash.addHash(id);
         render_stream_video(id);
+        console.log("retrieving information...");
+        window.history.pushState('', '', '/gamer_stream_multi');
     });
 
     get_streamers();
