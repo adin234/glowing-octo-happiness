67574ee9a516d2698a96a202f1e7d674ae4a574a
ronaldpabilonia <ron@any.tv>
2015-03-11 18:31:43 +0800

    fix config conflicts

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,3 @@
-/*var server = 'http://dev.gamers.tm:3000/'; //'http://api.gamers.tm/'; //;//http://api.gamers.tm/'
-var origin = 'http://dev.gamers.tm:8000/';*/
-
-
 var server = 'http://api.gamers.tm/'; //'http://api.gamers.tm/'; //;//http://api.gamers.tm/'
 var origin = 'http://beta.gamers.tm/'; //'http://localhost:8000/';  //'http://www.gamers.tm/';////
 var socket_server = 'http://api.gamers.tm:3001/';
