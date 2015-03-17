fdcfe95921f1f11ce8a1e6d53cb59272ddf52534
Ryan Navarroza <mataps47@gmail.com>
2015-03-17 18:53:57 +0800

    initially moved some markup to templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-var install = require("gulp-install");
-var gulp = require("gulp");
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
+var install = require("gulp-install"),
+    gulp = require("gulp"),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    template = require('gulp-template-compile'),
+    concat = require('gulp-concat'),
+    replace = require('gulp-replace');
 
 gulp.task('image-compressed', function () {
     return gulp.src('assets/images/**/*.*')
@@ -15,3 +18,14 @@ gulp.task('image-compressed', function () {
         .pipe(gulp.dest('assets/dist'));
 });
 
+gulp.task('template-compile', function() {
+    return gulp.src('assets/templates/**/*.html')
+        .pipe(template())
+        .pipe(concat('templates.js'))
+        .pipe(replace(/_.escape/g, '\'\''))
+        .pipe(gulp.dest('assets/js'));
+});
+
+gulp.task('dev', ['template-compile'], function() {
+    gulp.watch('assets/templates/**/*', ['template-compile']);
+});
