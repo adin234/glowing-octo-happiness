a7d6f1a6ee77fe17f7349d07b978c389e1b44138
ronaldpabilonia <ron@any.tv>
2015-03-18 21:03:28 +0800

    fix style

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 var server = 'http://api.gamers.tm/';
-var origin = 'http://beta.gamers.tm/';
+var origin = 'http://localhost:8000/';
 var socket_server = 'http://api.gamers.tm:3001/';
 var attachments_server = 'http://community.gamers.tm/zh/';
 var community = 'http://community.gamers.tm/zh/';
