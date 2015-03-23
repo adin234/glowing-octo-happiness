da41cd0e2b2fc7a9e13dedbf78df98e7d0eed7a7
nesse <jdeburce@gmail.com>
2015-03-23 16:36:49 +0800

    freedom activities update

--- assets/js/faactivities.js
+++ assets/js/faactivities.js
@@ -1,14 +1,48 @@
-'use strict';
-var all_events = [];
-$.get(server + "freedom_events", function (data) {
-    show_events(data);
-    add_events(data);
+$.get(server + "freedom_events/checkAdmin", function (data) {
+    'use strict';
+    show_html(data); //data for admin use only
+
 });
+
+$.get(server + "freedom_events", function (data) {
+    'use strict';
+    filter_display_events(data);
+});
+
+
+var show_html = function (data) {
+    'use strict';
+    var html = [];
+
+    $.ajax({
+            dataType: 'jsonp',
+            url: server + 'logged_user',
+            type: 'get'
+        })
+        .done(function (session) {
+            if (session.message === "Not logged in.") {
+                document.getElementById('all_events_form').innerHTML = "";
+            }
+            else {
+                $.get(server + "user/" + session.user_id, function (user) {
+                    if (user.is_admin === 1) {
+                        data.forEach(function (item) {
+                            html.push(item);
+                        });
+                        $('.add_events_form').html(html.join(''));
+                    }
+                });
+            }
+        });
+}
+
 
 var get_date_diff = function (sched, time) {
     var today = new Date(),
-        currdate = today.toJSON().substr(0, 10),
-        currtime = today.toTimeString().substr(0, 5),
+        currdate = today.toJSON()
+        .substr(0, 10),
+        currtime = today.toTimeString()
+        .substr(0, 5),
         result = currdate.localeCompare(sched),
         result2 = currtime.localeCompare(time);
 
@@ -28,9 +62,12 @@ var get_date_diff = function (sched, time) {
     }
 };
 
-var sched_template = function (all_events) {
+var show_events = function (data) {
+
+    'use strict';
     var html = [];
-    all_events.forEach(function (item) {
+
+    data.forEach(function (item) {
         html.push('<div class="activity">');
         html.push('<div class="left">');
         html.push('<div id="startEventDate">' + item.start_date + '</div>' +
@@ -49,119 +86,30 @@ var sched_template = function (all_events) {
         html.push('</div>');
         html.push('</div>');
     });
+
     return html;
-};
 
-var add_event = function () {
-    $.ajax({
+}
 
-        url: server + 'freedom_events/add',
-        type: 'post',
-        data: {
-            'event_title': $('#event_name').val(),
-            'start_date': $('#event_start_date').val(),
-            'end_date': $('#event_end_date').val(),
-            'start_time': $('#event_start_time').val(),
-            'end_time': $('#event_end_time').val(),
-            'e_description': $('#event_desc').val()
-        }
-    });
-};
 
-var show_events = function (all_events) {
+var filter_display_events = function (all_events) {
+
+    'use strict';
     all_events.forEach(function (item) {
         if ((get_date_diff(item.end_date, item.end_time) === 'Ongoing') || (get_date_diff(item.end_date,
                 item.end_time) === 'Ended')) {
-            var html_content_sched = sched_template(all_events);
-            $('#all_schedule').html(html_content_sched.join(''));
+            var html_content_sched = show_events(all_events);
+            $('#all_schedule')
+                .html(html_content_sched.join(''));
         }
     });
     all_events.forEach(function (item) {
         if (get_date_diff(item.end_date, item.end_time) === 'Ended') {
-            var html_content_archive = sched_template(all_events);
-            $('#archive_schedule').html(html_content_archive.join(''));
+            var html_content_archive = show_events(all_events);
+            $('#archive_schedule')
+                .html(html_content_archive.join(''));
         }
     });
-};
 
-var check_admin_user = function () {
-    var userinfo = $.parseJSON(utilCookie.get('user'));
-    var user_id = userinfo.user_id;
 
-    if (user_id === 18) {
-        return true;
-    }
-    else {
-        return false;
-    }
-};
-
-var add_event_form = function () {
-    var html = [];
-    html.push('<div id="add_event_header">Add an Event</div>');
-    html.push('<div id="addForm"><form>');
-    html.push(
-        '<p id="e_title">Title</p> <input type="text" name="event_name" placeholder="Event Title" id="event_name" required>'
-    );
-    html.push(
-        '<p id="e_s_date">Start Date</p> <input type = "date" name = "event_start_date"' +
-        'placeholder = "Event Start Date"' +
-        'id = "event_start_date" required>'
-    );
-    html.push(
-        '<p id="e_e_date">End Date</p> <input type="date"' +
-        'name="event_end_date" placeholder="Event End Date"' +
-        ' id="event_end_date" required>'
-    );
-    html.push(
-        '<p id="s_time">Start Time</p> <input type="time"' +
-        ' name="event_start_time"' +
-        'placeholder="Event Start Time" id="event_start_time" required>'
-    );
-    html.push(
-        '<p id="e_time">End Time</p> <input type="time"' +
-        'name="event_end_time"' +
-        'placeholder="Event End Time" id="event_end_time" required>'
-    );
-    html.push('<p id="e_desc">Event Description</p>');
-
-    html.push(
-        '<textarea id="event_desc" name="event_desc" placeholder="Event Description"></textarea>'
-    );
-    html.push('<button onclick="add_event()">ADD EVENT</button>');
-    $('.add_events_form').html(html.join(''));
-};
-
-var show_all_events = function (events) {
-    var html = [];
-    html.push('<div id="backButton">' + '<a href="' + origin + 'freedom/#tab-4-1">' +
-        '<img src="/assets/images/back_button.png"></a>' + '</div>');
-    events.forEach(function (item) {
-        html.push('<div id="title">' + item.event_title + '</div>');
-        html.push('<div id="startDate">' + item.start_date + '</div>');
-        html.push('<div id="endDate">' + item.end_date + '</div>');
-        html.push('<div id="startTime">' + item.start_time + '</div>');
-        html.push('<div id="endTime">' + item.end_time + '</div>');
-        html.push('<div id="e-desc">' + item.e_description + '</div>');
-        html.push('<div id="join-event">' +
-            '<button onclick="join_event()">JOIN EVENT</buttton>' +
-            '</div>');
-        html.push('<div id="join-link">' + '<p>ENTER JOIN EVENT LINK</p>' +
-            '<input type="text" name="event_link" id="event_link">' + '</div>');
-    });
-    $('#show_events').html(html.join(''));
-    $('#editEvent').html('<div id="edit_event">' +
-        '<button onclick="update_events()"><img src="/assets/images/pencil.jpg">EDIT EVENT</button>' +
-        '</div>'
-    );
-};
-
-var add_events = function (all_events) {
-    if (check_admin_user() === true) {
-        add_event_form();
-    }
-    else if (check_admin_user() === false) {
-        show_all_events(all_events);
-    }
-};
-
+}
