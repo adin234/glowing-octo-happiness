c08c7e59878d8307d4aff25df4b58feff1d7adf4
nesse <jdeburce@gmail.com>
2015-03-26 19:47:04 +0800

    stash changes

--- assets/js/faactivities.js
+++ assets/js/faactivities.js
@@ -1,33 +1,27 @@
-$.get(server + "freedom_events/checkAdmin", function (data) {
-    'use strict';
-    console.log(data);
+'use strict';
+/*global server, $*/
+$.get(server + 'freedom_events/checkAdmin', function(data) {
     show_html(data); //data for admin use only
-
 });
-
-$.get(server + "freedom_events", function (data) {
-    'use strict';
+$.get(server + 'freedom_events', function(data) {
     filter_display_events(data);
 });
 
 
-var show_html = function (data) {
-    'use strict';
+var show_html = function(data) {
     var html = [];
-
     $.ajax({
             dataType: 'jsonp',
             url: server + 'logged_user',
             type: 'get'
         })
-        .done(function (session) {
-            if (session.message === "Not logged in.") {
-                document.getElementById('all_events_form').innerHTML = "";
-            }
-            else {
-                $.get(server + "user/" + session.user_id, function (user) {
+        .done(function(session) {
+            if (session.message === 'Not logged in.') {
+                document.getElementById('all_events_form').innerHTML = '';
+            } else {
+                $.get(server + 'user/' + session.user_id, function(user) {
                     if (user.is_admin === 1) {
-                        data.forEach(function (item) {
+                        data.forEach(function(item) {
                             html.push(item);
                         });
                         $('.add_events_form').html(html.join(''));
@@ -35,10 +29,9 @@ var show_html = function (data) {
                 });
             }
         });
-}
+};
 
-
-var get_date_diff = function (sched, time) {
+var get_date_diff = function(sched, time) {
     var today = new Date(),
         currdate = today.toJSON()
         .substr(0, 10),
@@ -49,26 +42,22 @@ var get_date_diff = function (sched, time) {
 
     if (result < 0) {
         return 'Ongoing';
-    }
-    else if (result === 0) {
+    } else if (result === 0) {
         if (result2 <= 0) {
             return 'Ongoing';
-        }
-        else {
+        } else {
             return 'Ended';
         }
-    }
-    else {
+    } else {
         return 'Ended';
     }
 };
 
-var show_events = function (data) {
+var show_events = function(data) {
 
-    'use strict';
     var html = [];
 
-    data.forEach(function (item) {
+    data.forEach(function(item) {
         html.push('<div class="activity">');
         html.push('<div class="left">');
         html.push('<div id="startEventDate">' + item.start_date + '</div>' +
@@ -90,13 +79,13 @@ var show_events = function (data) {
 
     return html;
 
-}
+};
 
 
-var filter_display_events = function (all_events) {
+var filter_display_events = function(all_events) {
 
-    'use strict';
-    all_events.forEach(function (item) {
+
+    all_events.forEach(function(item) {
         if ((get_date_diff(item.end_date, item.end_time) === 'Ongoing') || (get_date_diff(item.end_date,
                 item.end_time) === 'Ended')) {
             var html_content_sched = show_events(all_events);
@@ -104,12 +93,11 @@ var filter_display_events = function (all_events) {
                 .html(html_content_sched.join(''));
         }
     });
-    all_events.forEach(function (item) {
+    all_events.forEach(function(item) {
         if (get_date_diff(item.end_date, item.end_time) === 'Ended') {
             var html_content_archive = show_events(all_events);
             $('#archive_schedule')
                 .html(html_content_archive.join(''));
         }
     });
-}
-
+};
