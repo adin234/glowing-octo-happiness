0b75827b7bc1296730d7fb51310d115752cfc692
Ryan Navarroza <mataps47@gmail.com>
2015-04-02 15:22:51 +0800

    initial optim build

--- gulpfile.js
+++ gulpfile.js
@@ -69,38 +69,55 @@ gulp.task('compile-js', function() {
     var requirejs = require('requirejs');
 
     var config = {
+
         baseUrl: 'assets/js',
-        dir: 'assets/js/dist',
-        map: {
-            '*': {
-                'less': 'libs/require-less/less'
-            }
-        },
+        name: 'components/index/main',
+        out: 'assets/dist/main-built.js',
         paths: {
             'jquery': 'libs/jquery.min',
             'text': 'libs/text',
             'css': '../css',
-            'common': 'components/common'
-        },
-        shim: {
-            'util'                                      : ['jquery'],
-            'function'                                  : ['jquery'],
-            'libs/jquery.gritter.min'                   : ['jquery'],
-            'libs/hoverIntent'                          : ['jquery'],
-            'libs/superfish'                            : ['jquery'],
-            'libs/jquery.fixed.menu'                    : ['jquery'],
-            'libs/jquery.autocomplete.min'              : ['jquery'],
-            'libs/jquery.bxslider.min'                  : ['jquery'],
-            'libs/jquery.mCustomScrollbar.concat.min'   : ['jquery'],
-            'libs/jquery.tabslet.min'                   : ['jquery'],
-            'libs/jquery.tooltipster.min'               : ['jquery'],
-            'libs/socketio'                             : ['jquery']
-        },
-        modules: [
-            {
-                name: 'components/index/main'
-            }
-        ]
+            'common': 'components/common',
+            'less-builder': 'libs/require-less/less-builder',
+            'normalize': 'libs/require-less/normalize',
+            'less': 'libs/require-less/less'
+        }
+
+
+        /*MODULE CONFIG*/
+        // baseUrl: 'assets/js',
+        // dir: 'assets/js/dist',
+        // map: {
+        //     '*': {
+        //         'less': 'libs/require-less/less'
+        //     }
+        // },
+        // paths: {
+        //     'jquery': 'libs/jquery.min',
+        //     'text': 'libs/text',
+        //     'css': '../css',
+        //     'common': 'components/common'
+        // },
+        // shim: {
+        //     'util'                                      : ['jquery'],
+        //     'function'                                  : ['jquery'],
+        //     'libs/jquery.gritter.min'                   : ['jquery'],
+        //     'libs/hoverIntent'                          : ['jquery'],
+        //     'libs/superfish'                            : ['jquery'],
+        //     'libs/jquery.fixed.menu'                    : ['jquery'],
+        //     'libs/jquery.autocomplete.min'              : ['jquery'],
+        //     'libs/jquery.bxslider.min'                  : ['jquery'],
+        //     'libs/jquery.mCustomScrollbar.concat.min'   : ['jquery'],
+        //     'libs/jquery.tabslet.min'                   : ['jquery'],
+        //     'libs/jquery.tooltipster.min'               : ['jquery'],
+        //     'libs/socketio'                             : ['jquery']
+        // },
+        // modules: [
+        //     {
+        //         name: 'components/index/main',
+        //         exclude: ['libs/require-less/normalize']
+        //     }
+        // ]
     };
 
     return requirejs.optimize(config, function (buildResponse) {
