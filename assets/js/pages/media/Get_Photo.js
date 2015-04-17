a0574a23bd7676ea0b94b41fe9cfb7dd953047ed
Ryan Navarroza <mataps47@gmail.com>
2015-04-17 17:12:20 +0800

    fixed user image display

--- assets/js/pages/media/Get_Photo.js
+++ assets/js/pages/media/Get_Photo.js
@@ -6,7 +6,7 @@ define(
             $.getJSON('http://gdata.youtube.com/feeds/api/users/' + id.substr(2) +
                 '?fields=yt:username,media:thumbnail,title&alt=json', {},
                 function(e) {
-                    $(context).attr('src', e['entry.media$thumbnail.url']);
+                    context.attr('src', e.entry.media$thumbnail.url);
                 }
             );
         };
