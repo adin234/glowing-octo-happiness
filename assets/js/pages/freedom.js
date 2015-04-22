07f3b54b38152a3e02f56019e1685887ada6efe3
Ryan Navarroza <mataps47@gmail.com>
2015-04-22 17:18:09 +0800

    added separate tabs

---
+++ assets/js/pages/freedom.js
@@ -0,0 +1,28 @@
+'use strict';
+
+define('freedom', function(require) {
+
+	var Tabs = require('Tabs/index'),
+		tab_nav_tpl = require('text!./templates/freedom-tab-nav.html'),
+		tab1_tpl = require('text!./templates/freedom-tab1.html'),
+		tab2_tpl = require('text!./templates/freedom-tab2.html'),
+		tab3_tpl = require('text!./templates/freedom-tab3.html'),
+		main_tab = new Tabs({
+			className: 'lanparty_type1 adjust_line_green',
+			template: tab_nav_tpl
+		});
+
+	main_tab
+		.init()
+		.addTab('schedule', 'Freedom! 活動時間', 'FreedomSchedule', tab1_tpl)
+        .addTab('activities', '活動內容', 'FreedomEvents', tab2_tpl)
+        .addTab('archive', 'Freedom!活動紀錄', 'FreedomArchive', tab3_tpl)
+        .mount($('#container_lanparty_green'));
+
+    $('#container_lanparty_green').prepend('<hr class="line"/>');
+
+	require('Footer/index');
+    require('Sub_Nav/index');
+});
+
+require(['freedom']);
