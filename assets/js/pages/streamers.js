61f9bf654d92d2e36dc90ad3b0c793af0d34bdc6
Ryan Navarroza <mataps47@gmail.com>
2015-05-05 16:00:55 +0800

    moved initialization of component outside socket message

--- assets/js/pages/streamers.js
+++ assets/js/pages/streamers.js
@@ -13,6 +13,106 @@ define('streamers', function(require) {
     page_data._live = [];
     page_data._lanparty = [];
     page_data._multiview = [];
+    // 
+    // page_data = {
+    //     "streamers":[
+    //         {
+    //             "user_id":1757,
+    //             "field_id":"twitchStreams",
+    //             "field_value":[  
+    //                 "sing_sing"
+    //             ],
+    //             "username":"barney",
+    //             "email":"barneyeureka@mailtothis.com",
+    //             "gender":"male",
+    //             "custom_title":"",
+    //             "language_id":1,
+    //             "style_id":0,
+    //             "timezone":"Asia/Hong_Kong",
+    //             "visible":1,
+    //             "user_group_id":2,
+    //             "secondary_group_ids":"",
+    //             "display_style_group_id":2,
+    //             "permission_combination_id":2,
+    //             "message_count":2,
+    //             "conversations_unread":0,
+    //             "register_date":1418626633,
+    //             "last_activity":1421399731,
+    //             "trophy_points":1,
+    //             "alerts_unread":1,
+    //             "avatar_date":1418626633,
+    //             "avatar_width":200,
+    //             "avatar_height":125,
+    //             "gravatar":"",
+    //             "user_state":"valid",
+    //             "is_moderator":0,
+    //             "is_admin":0,
+    //             "is_banned":0,
+    //             "like_count":0,
+    //             "warning_points":0,
+    //             "is_staff":0,
+    //             "activity_visible":0,
+    //             "twitch":{  
+    //                 "_id":14290360528,
+    //                 "game":"Dota 2",
+    //                 "viewers":13701,
+    //                 "created_at":"2015-05-05T01:21:28Z",
+    //                 "video_height":1080,
+    //                 "average_fps":59.9835240275,
+    //                 "_links":{  
+    //                     "self":"https://api.twitch.tv/kraken/streams/sing_sing"
+    //                 },
+    //                 "preview":{  
+    //                     "small":"http://static-cdn.jtvnw.net/previews-ttv/live_user_sing_sing-80x45.jpg",
+    //                     "medium":"http://static-cdn.jtvnw.net/previews-ttv/live_user_sing_sing-320x180.jpg",
+    //                     "large":"http://static-cdn.jtvnw.net/previews-ttv/live_user_sing_sing-640x360.jpg",
+    //                     "template":"http://static-cdn.jtvnw.net/previews-ttv/live_user_sing_sing-{width}x{height}.jpg"
+    //                 },
+    //                 "channel":{  
+    //                     "_links":{  
+    //                         "self":"http://api.twitch.tv/kraken/channels/sing_sing",
+    //                         "follows":"http://api.twitch.tv/kraken/channels/sing_sing/follows",
+    //                         "commercial":"http://api.twitch.tv/kraken/channels/sing_sing/commercial",
+    //                         "stream_key":"http://api.twitch.tv/kraken/channels/sing_sing/stream_key",
+    //                         "chat":"http://api.twitch.tv/kraken/chat/sing_sing",
+    //                         "features":"http://api.twitch.tv/kraken/channels/sing_sing/features",
+    //                         "subscriptions":"http://api.twitch.tv/kraken/channels/sing_sing/subscriptions",
+    //                         "editors":"http://api.twitch.tv/kraken/channels/sing_sing/editors",
+    //                         "videos":"http://api.twitch.tv/kraken/channels/sing_sing/videos",
+    //                         "teams":"http://api.twitch.tv/kraken/channels/sing_sing/teams"
+    //                     },
+    //                     "background":null,
+    //                     "banner":"http://static-cdn.jtvnw.net/jtv_user_pictures/sing_sing-channel_header_image-b59c20a951432198-640x125.png",
+    //                     "broadcaster_language":"ru",
+    //                     "display_name":"Sing_sing",
+    //                     "game":"Dota 2",
+    //                     "logo":"http://static-cdn.jtvnw.net/jtv_user_pictures/sing_sing-profile_image-260bffe71ab9475b-300x300.jpeg",
+    //                     "mature":true,
+    //                     "status":"newbioe strem",
+    //                     "partner":true,
+    //                     "url":"http://www.twitch.tv/sing_sing",
+    //                     "video_banner":null,
+    //                     "_id":21390470,
+    //                     "name":"sing_sing",
+    //                     "created_at":"2011-03-28T14:51:57Z",
+    //                     "updated_at":"2015-05-05T03:15:35Z",
+    //                     "delay":0,
+    //                     "followers":201750,
+    //                     "profile_banner":null,
+    //                     "profile_banner_background_color":null,
+    //                     "views":33651832,
+    //                     "language":"en"
+    //                 }
+    //             }
+    //         }
+    //     ],
+    //     "_live":[],
+    //     "_lanparty":[],
+    //     "_multiview":[],
+    //     "youtube":[],
+    //     "hitbox":[],
+    //     "twitch":[]
+    // };
 
     var Tabs = require('../components/Tabs/index'),
         List_Slider = require('../components/List_Slider/index'),
@@ -200,18 +300,7 @@ define('streamers', function(require) {
         };
 
        
-    socket.on('message', function(e) {
-        separate_lan_party_streams(e.streamers);
-        if (!live_mounted) {
-            live_slider.init(page_data._live)
-                .mount($('#container-videos'));
-            live_mounted = true;
-        } else {
-            live_slider.reload(page_data._live);
-        }
 
-        lanparty_slider.reload(page_data._lanparty);
-    });
 
     main_tab
         .init()
@@ -231,14 +320,25 @@ define('streamers', function(require) {
     );
 
     multiview_slider
-        .init(page_data._multiview)
+        .init([])
         .mount($('#container-multiview'));
 
     lanparty_slider
-        .init(page_data._lanparty)
+        .init([])
         .mount($('#container-lanparty'));
 
-    
+    live_slider.init([])
+        .mount($('#container-videos'));
+
+
+    socket.on('message', function(e) {
+        separate_lan_party_streams(e.streamers);
+
+        live_slider.reload(page_data._live);
+        lanparty_slider.reload(page_data._lanparty);
+    });
+
+
     $('#txtbox-search-videos').autocomplete({
         lookup: function(query, done) {
             var result = page_data._live.filter(function(item) {
