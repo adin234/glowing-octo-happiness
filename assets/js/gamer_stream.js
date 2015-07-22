a2e26e466497688c67f424370a775869b12b077b
Ryan Navarroza <mataps47@gmail.com>
2015-07-22 18:21:34 +0800

    refactored gamers stream page

--- assets/js/gamer_stream.js
+++ assets/js/gamer_stream.js
@@ -1,22 +1,176 @@
-/* jshint unused: false */
 /* global
     server,
     template,
     twitch,
     page_data: true,
     JST,
-    page_maintenance,
     Masonry,
-    params,
-    utilCookie,
-    XBBCODE,
-    community
+    params
 */
 
 'use strict';
 
 var streamerId,
-  check_if_online = function (userId) {
+    viewers = 0,
+    
+    information_masonry = function() {
+        $('#tab-2').width($('#streamArea aside').width() - $('.streamer').width() - 40);
+
+        new Masonry(
+            '#tab-2',
+            {
+                columnWidth: 350,
+                itemSelector: '.panel'
+            }
+        );
+    },
+    
+    render_stream = function(streamType, streamId) {
+        viewers = 0;
+
+        if (streamType === 'TW') {
+            getViewersCount(server + 'get_views/' + twitch);
+
+            $('#twitchStream').replaceWith(template(
+                JST['twitch-stream-tpl.html'](),
+                { twitchid: streamId, number: viewers }
+            ));
+
+            $('#twitchTalk').html(template(
+                JST['twitch-chat-tpl.html'](),
+                {
+                    twitchid: streamId,
+                    advert: page_data.custom_fields && page_data.custom_fields.advertisement
+                }
+            ));
+
+            $('object').height(
+                $('#streamView').height()
+            );
+        }
+
+        //if we got hitbox
+        if (streamType === 'HB') {
+
+            getViewersCount(server + 'get_hitbox_views/' + streamId);
+
+            $('#twitchStream').replaceWith(template(
+                JST['hitbox-stream-tpl.html'](),
+                { twitchid: streamId, number: viewers }
+            ));
+
+            $('#twitchTalk').html(template(
+                JST['hitbox-chat-tpl.html'](),
+                {
+                    twitchid: streamId,
+                    advert: page_data.custom_fields && page_data.custom_fields.advertisement
+                }
+            ));
+
+            $('object').height( $('#streamView').height() );
+
+            $.getJSON(server + 'scrape/' + streamId, function(e) {
+                e.forEach(function(item) {
+                    item.data.html = item.html_description;
+                    item.data.link = item.data.link || '';
+                    $('#tab-2').append(template(JST['panelTpl.html'](), item.data));
+                });
+
+                setTimeout(function() {
+                    $('img[src=""]').hide();
+                }, 100);
+            });
+        }
+
+        if (streamType === 'YT') {
+
+            getViewersCount(server + 'get_views/' + twitch);
+
+            var found = false,
+                userId = params.user.replace('/', '');
+
+            $.getJSON(server + 'streamers/youtube/?user=' + userId, function(e) {
+                var streamerId = '';
+
+                e.streamers.forEach(function(item) {
+                    found = true;
+                    streamerId = item.youtube.id;
+
+                    $('.streamer #about-streamer').html(
+                        item.youtube.snippet.description.replace(/(?:\r\n|\r|\n)/g, '<br />')
+                    );
+
+                    $('embed').height( $('#streamView').height() );
+                });
+
+                if (found) {
+                    $('#twitchStream')
+                        .replaceWith(template(
+                            JST['youtube-stream-tpl.html'](), {youtubeid: streamerId})
+                        );
+                }
+                else {
+                    $('#twitchStream')
+                        .replaceWith(
+                                '<div id="twitchStream">'+
+                                        '<img class="offline-placeholder" ' +
+                                            'src="/assets/images/streamer-offline.png"/>'+
+                                '</div>'
+                        );
+                }
+
+                if (!found) {
+                    $('aside .streamer').hide();
+                }
+
+                $('#tab-2').html(page_data.custom_fields.youtube_activity);
+            });
+
+            var userinfo = '',
+                channelinfo = { id: twitch, title: twitch };
+
+            if (utilCookie.get('user').length > 0) {
+                userinfo = $.parseJSON(utilCookie.get('user'));
+                $('#twitchTalk').initChatBox(channelinfo, userinfo, 'gamers-stream');
+            } else {
+                $('#twitchTalk').initChatBox(channelinfo, userinfo, 'gamers-stream');
+            }
+        }
+    },
+
+    resize_video_stream = function () {
+        var size = $('#view-resize').val();
+        if (size === 'x3') {
+            $('embed').height($('#streamView').height());
+            $('object').height($('#streamView').height());
+        } else {
+            $('embed').height('100%');
+            $('object').height('100%');
+        }
+    },
+
+    updateStatus = function(streamType, streamId, isOnline) {
+        if (isOnline) {
+            render_stream(streamType, streamId);
+        }
+        else {
+            $('#twitchStream')
+                .replaceWith(
+                    '<div id="twitchStream"><img class="offline-placeholder" src="/assets/images/streamer-offline.png"/></div>'
+                );
+        }
+    },
+
+    getViewersCount = function (url) {
+        $.getJSON(url, function(e) {
+            if (e && e.stream && e.stream.viewers) {
+                viewers = e.stream.viewers || 0;
+                $('#twitchStream .views').html(e.stream.viewers + ' Views');
+            }
+        });
+    },
+
+    check_if_online = function (userId) {
         var found = false;
         $.getJSON(server + 'streamers/youtube/?user=' + userId, function(e) {
             e.streamers.forEach(function(item) {
@@ -26,8 +180,11 @@ var streamerId,
 
             if (found) {
                 $('#twitchStream')
-                    .replaceWith(template($('#youtube-stream-tpl')
-                    .html(),{youtubeid: streamerId}));
+                    .replaceWith(template(
+                                $('#youtube-stream-tpl').html(),
+                                { youtubeid: streamerId }
+                    ));
+
                 setTimeout(function() {
                     check_if_online(userId);
                 }, 30000);
